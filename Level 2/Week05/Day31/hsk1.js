42d8f5544eb94a7b9599177c7e4cbc68f4da2f4b
waterglasses <hsk10271@gmail.com>
2023-04-05 23:04:05 +0900

    fail: 황수경 31일차 순위 검색 문제

---
+++ Level 2/Week05/Day31/hsk1.js
@@ -0,0 +1,13 @@
+function solution(info, query) {
+  var answer = [];
+  return answer;
+}
+
+console.log([
+  'java backend junior pizza 150',
+  'python frontend senior chicken 210',
+  'python frontend senior chicken 150',
+  'cpp backend senior pizza 260',
+  'java backend junior chicken 80',
+  'python backend senior chicken 50',
+]);
