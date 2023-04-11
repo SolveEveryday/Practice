46178683c1208a8e890ab08b31b906e9184533a2
waterglasses <hsk10271@gmail.com>
2023-04-11 22:49:30 +0900

    solve: 황수경 37일차 문제 풀이

---
+++ Level 2/Week06/Day37/hsk1.js
@@ -0,0 +1,11 @@
+function solution(n) {
+  let fibonacci = [0, 1];
+  for (let i = 2; i <= n; i++) {
+    fibonacci[i] = (fibonacci[i - 1] + fibonacci[i - 2]) % 1234567;
+  }
+
+  return fibonacci[n] % 1234567;
+}
+
+console.log(solution(3));
+console.log(solution(5));
